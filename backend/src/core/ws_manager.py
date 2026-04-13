ce951f2b2ebec76ae178070783ce542f7c42d6a9
Zeynep Gul Acar <acarthegul19@gmail.com>
2026-04-13 11:06:35 +0300

    websocket updated

--- backend/src/core/ws_manager.py
+++ backend/src/core/ws_manager.py
@@ -3,28 +3,29 @@ from typing import Dict, List
 
 class ConnectionManager:
     def __init__(self):
-        # Hangi şirket ID'sinde hangi WebSocket bağlantıları (kullanıcılar) açık onu tutarız
+        # Workspace ID bazlı bağlantıları tutar
         self.active_connections: Dict[int, List[WebSocket]] = {}
 
-    async def connect(self, websocket: WebSocket, company_id: int):
+    async def connect(self, websocket: WebSocket, workspace_id: int):
         await websocket.accept()
-        if company_id not in self.active_connections:
-            self.active_connections[company_id] = []
-        self.active_connections[company_id].append(websocket)
+        if workspace_id not in self.active_connections:
+            self.active_connections[workspace_id] = []
+        self.active_connections[workspace_id].append(websocket)
 
-    def disconnect(self, websocket: WebSocket, company_id: int):
-        if company_id in self.active_connections:
-            if websocket in self.active_connections[company_id]:
-                self.active_connections[company_id].remove(websocket)
-            # Eğer şirketteki son analist de çıkış yaptıysa listeyi temizle
-            if not self.active_connections[company_id]:
-                del self.active_connections[company_id]
+    def disconnect(self, websocket: WebSocket, workspace_id: int):
+        if workspace_id in self.active_connections:
+            if websocket in self.active_connections[workspace_id]:
+                self.active_connections[workspace_id].remove(websocket)
+            if not self.active_connections[workspace_id]:
+                del self.active_connections[workspace_id]
 
-    async def broadcast_to_company(self, message: dict, company_id: int):
-        # Sadece ilgili şirketin açık ekranlarına alarmı fırlat!
-        if company_id in self.active_connections:
-            for connection in self.active_connections[company_id]:
-                await connection.send_json(message)
+    async def broadcast_to_workspace(self, message: dict, workspace_id: int):
+        if workspace_id in self.active_connections:
+            for connection in self.active_connections[workspace_id]:
+                try:
+                    await connection.send_json(message)
+                except Exception:
+                    # Bağlantı kopmuşsa temizle
+                    pass
 
-# Tüm projenin kullanacağı tek bir yönetici nesnesi oluşturuyoruz
 manager = ConnectionManager()
