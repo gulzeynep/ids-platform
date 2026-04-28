1f21149291e5b1f5b2d827d0abc62a9d85dc25e9
Zeynep Gul Acar <acarthegul19@gmail.com>
2026-04-28 14:22:37 +0300

    added password validator

--- backend/src/schemas.py
+++ backend/src/schemas.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel, EmailStr, field_validator
 from typing import Optional
 from datetime import datetime
+import re 
 
 # Auth and User Schemas
 class UserRegister(BaseModel):
-    """Just Email and Password"""
     email: EmailStr
     password: str
 
@@ -12,6 +12,20 @@ class UserRegister(BaseModel):
     @classmethod
     def trim_and_lower(cls, v: str) -> str:
         return v.strip().lower()
+    
+    @field_validator('password')
+    @classmethod
+    def validate_password(cls, v:str) -> str:
+        if len(v) < 8:
+            raise ValueError("Password must be at least 8 characters long.")
+        if not re.search(r"[A-Z]", v):
+            raise ValueError("Password must contain at least one uppercase letter.")
+        if not re.search(r"[a-z]", v):
+            raise ValueError("Password must contain at least one lowercase letter.")
+        if not re.search(r"[0-9]", v):
+            raise ValueError("Password must contain at least one digit.")
+        if not re.search(r"[!@#$%^&*()-+_.]", v):
+            raise ValueError("Password must contain at least one special character.")
 
 class UserProfileUpdate(BaseModel):
     """Onboarding Setup"""
